b1c2405ea19d78ca429c1955874ae9d563466547
morgane quere <qu.morgane@gmail.com>
2021-04-16 14:12:46 +0200

    adding some style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state= { term: '' };
+    
     render() {
         return (
-          <div>
-            <form>
-              <input type='text' />
+          <div className="ui segment">
+            <form className="ui form">
+              <div className="field">
+                <lable>Image search</lable>
+                <input 
+                  type='text'
+                  value={this.state.term}
+                  onChange={ (event) => this.setState({ term: event.target.value})}
+                />
+              </div>
             </form>
           </div>
         );
