a0f7187b572faa1f92e8b6a5dfa95111011f6e34
morgane quere <qu.morgane@gmail.com>
2021-04-20 16:47:47 +0200

    added my env file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   
   onSearchSubmit(term) {
-    console.log(term);
+    const url = 'https://api.unsplash.com/search/photos';
+    const client_id = process.env.REACT_APP_PICS_API_KEY;
+    console.log(client_id);
+
+    /*axios.get(url, {
+
+    });*/
   }
 
   render() {
